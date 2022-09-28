e33fdc3d4d910bc058ffdf738c6a55159aebb81b
Kyurre <52509811+Kyurre@users.noreply.github.com>
2022-09-28 12:20:38 -0500

    Changes to seLinkList and Player.java

--- Assignment2/Player.java
+++ Assignment2/Player.java
@@ -3,6 +3,39 @@ package Assignment2;
 public class Player{
 
     public int numPlayers;
+    SeLinkList head;
+    private PlayerData[] data;
+    private PlayerData player;
+
+
+
+
+
+
+    public void addPlayer(int a, int b, int c){
+        SeLinkList node = new SeLinkList(a, b, c);
+        if(head == null){
+            head = node;
+        }
+        else{
+            SeLinkList temp = head;
+            while(temp.next != null){
+                temp = temp.next;
+            }
+
+            temp.next = node;
+        }
+    }
+
+    public void display(){
+        SeLinkList node = head; 
+        while(node.next != null){
+            System.out.printf("%d %d %d\n", node.a, node.b, node.c);
+            node = node.next;
+        }
+        // print the final node
+        System.out.printf("%d %d %d\n", node.a, node.b, node.c);
+    }
 
 
     public static void main(String[] args){
@@ -13,7 +46,16 @@ public class Player{
         //Task 4
         PlayerData playerdata = new PlayerData();
         PlayerData[] myData = playerdata.getMyData();
-        System.out.println(myData);
+
+        for(PlayerData p: myData ){
+            System.out.println(p.a + " " + p.b + " " + p.c);
+        }
+
+        //Task7 Test
+        play.addPlayer(1, 5, 6);
+        play.addPlayer(2, 4, 6);
+        play.display();
+        
     }
     
 }
--- Assignment2/seLinkList.java
+++ Assignment2/seLinkList.java
@@ -1,18 +1,72 @@
 package Assignment2;
 
 public class SeLinkList {
-    public SeLinkList next;
+    
     public int a, b, c;
+    public SeLinkList next;
 
+    
     public SeLinkList(int a, int b, int c){
         this.a = a;
         this.b = b;
         this.c = c;
-        next = null;
+        this.next = next;
+    }
+    
+    /*
+    Node head; 
+   
+    static class Node {
+   
+        int a, b, c;
+        Node next;
+   
+        // Constructor
+        public Node(int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.next = null;
+        }
     }
 
+
+    public SeLinkList insert(SeLinkList list, int a, int b, int c){
+        Node node = new Node(a, b, c);
+
+        if (list.head == null){
+            list.head = node;
+        }else {
+            Node last = list.head;
+            while(last.next != null){
+                last = last.next;
+            }
+
+            last.next = node;
+        }
+
+        return list;
+    }
+    
+    public void printList(SeLinkList list){
+        Node curr = list.head;
+        System.out.println("Data in the list: ");
+
+        while(curr != null){
+            System.out.print(curr.a +  " " + curr.b + " " + curr.c + "\n");
+
+            curr = curr.next;
+        }
+    }
+    */
+
+
     public static void main(String[] args){
-        SeLinkList list = new SeLinkList (1, 5, 10);
+        SeLinkList list = new SeLinkList(1, 5, 10);
         System.out.println("Current list data: a) " + list.a + " b) " + list.b + " c) " + list.c);
+        //list.insert(list, 5, 10, 20);
+        //list.insert(list, 3, 4, 5);
+        //printList(list);
     }
 }
